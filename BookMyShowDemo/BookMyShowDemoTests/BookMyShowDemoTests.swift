e8af2f58e1384b6206f01978141169f7eba63e01
Aakash Narkar <aakash.narkar@neosoftmail.com>
2023-01-08 14:07:38 +0530

    feat(unit_testing): unit test for api response

--- BookMyShowDemo/BookMyShowDemoTests/BookMyShowDemoTests.swift
+++ BookMyShowDemo/BookMyShowDemoTests/BookMyShowDemoTests.swift
@@ -9,28 +9,23 @@ import XCTest
 @testable import BookMyShowDemo
 
 final class BookMyShowDemoTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_apiResource_with_validRequest_return_apiResponse() {
+        let service = ServiceAPI()
+        
+        let expectation = self.expectation(description: "validRequest_return_apiResponse")
+        
+        service.getMatchDetails(apiMethod: .getStudentData) { ressponse in
+            switch ressponse {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertTrue(data.count > 0)
+            case .failure(let error):
+                XCTAssertNil(error)
+            }
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 5)
     }
-
 }
